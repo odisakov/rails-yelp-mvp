12e200bb48bd6568b81e1ade06f8e317234c0eca
Oleg Isakov <odisakov@gmail.com>
2017-11-15 12:19:57 +0100

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,8 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   validates_presence_of :name
   validates :address, presence: true
-  validates :category, presence: true
+  validates :category, presence: true, inclusion: { in: %w(chinese italian japanese french belgian)}
 end
+
+
