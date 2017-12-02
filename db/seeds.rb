ecc3b78bc3b7de0342873075094f3cb3cf15b665
Oleg Isakov <odisakov@gmail.com>
2017-12-02 15:36:02 +0100

    rails new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-require 'faker'
-
-10.times do
-  r = Restaurant.create!(
-    name: Faker::App.name,
-    address: Faker::Address.street_address,
-    phone_number: Faker::PhoneNumber.phone_number,
-    category: %w(Chinese Italian Japanese French Belgian).sample,
-    )
-
-    3.times do
-      r.reviews.create!(
-        content: Faker::Seinfeld.quote,
-        rating: (1..5).to_a.sample,
-        )
-    end
-end
-
